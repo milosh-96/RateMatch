ec8739a5e9256ac1f297a3c119c94e823447282f
Miloš Jovanović <milosh-96@outlook.com>
2023-01-14 04:29:31 +0100

    hide sensitive info and utilize secrets json

--- RateMatch.Mvc/Program.cs
+++ RateMatch.Mvc/Program.cs
@@ -6,7 +6,7 @@ using RateMatch.Mvc.Data.IdentityEntities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration["ConnectionStrings:DevConnection"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
