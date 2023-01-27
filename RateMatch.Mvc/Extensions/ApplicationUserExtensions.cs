326b37601f81d4fd2837690f80c317e3205b2656
Miloš Jovanović <milosh-96@outlook.com>
2023-01-27 04:11:24 +0100

    work on create review

---
+++ RateMatch.Mvc/Extensions/ApplicationUserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using RateMatch.Mvc.Data.IdentityEntities;
+
+namespace RateMatch.Mvc.Extensions
+{
+    public static class ApplicationUserExtensions
+    {
+        public static ApplicationUserDto? ToDto(this ApplicationUser user)
+        {
+           if(user != null)
+            {
+                return new ApplicationUserDto()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Email = user.Email
+                };
+            }
+            return null;
+        }
+    }
+}
