7cc6657423434fd632ba061baba647c08993d5c0
Miloš Jovanović <milosh-96@outlook.com>
2023-02-09 04:01:11 +0100

    add competitions api controller

---
+++ RateMatch.Mvc/Areas/Api/Controllers/CompetitionsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RateMatch.Mvc.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RateMatch.Mvc.Areas.Api.Controllers
+{
+    [Area("Api")]
+    [Route("{area}/[controller]")]
+    [ApiController]
+    public class CompetitionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompetitionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CompetitionController>
+        [HttpGet]
+        public List<Competition> Get()
+        {
+            return _context.Competitions.ToList();
+        }
+
+        // GET api/<CompetitionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CompetitionController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CompetitionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CompetitionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- RateMatch.Mvc/Data/Competition.cs
+++ RateMatch.Mvc/Data/Competition.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace RateMatch.Mvc.Data
 {
@@ -12,9 +13,12 @@ namespace RateMatch.Mvc.Data
 
         [Column(TypeName = "varchar(256)")]
         public string Slug { get; set; } = "";
+
+        [JsonIgnore]
         public Sport? Sport { get; set; }
         public int? SportId { get; set; }
 
+        [JsonIgnore]
         public Country? Country { get; set; }
         public int? CountryId { get; set; }
 
