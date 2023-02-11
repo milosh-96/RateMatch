39a3e98c65a3787311bf3a1d57d9ee47291266fe
Miloš Jovanović <milosh-96@outlook.com>
2023-02-11 04:08:08 +0100

    add google analytics if production

--- RateMatch.Mvc/Program.cs
+++ RateMatch.Mvc/Program.cs
@@ -39,6 +39,8 @@ else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseCookiePolicy();
+    app.UseStatusCodePages();
 }
 //app.UseStatusCodePagesWithRedirects("/Error/{0}");
 app.UseHttpsRedirection();
